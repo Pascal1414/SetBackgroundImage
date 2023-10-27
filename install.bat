adb8bf7264ae990f49493ccebbbc4e2bf0fd93e5
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-10-27 11:47:16 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,21 @@
+:: Install requests package
 pip install requests
-pip install Pillow
+
+:: Create startup script
+
+set "appdataFolder=%APPDATA%"
+set "startupPath=%appdataFolder%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+:: Current path
+set "batchFilePath=%~dp0"
+
+set "batchFileContent=py %batchFilePath%startup.py"
+
+set "batchFileName=StartupScript.bat"
+(
+  echo @echo off
+  echo %batchFileContent%
+) > "%startupPath%\%batchFileName%"
+
+:: Run python script
+py startup.py
